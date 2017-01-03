9487edf954f380ba8a56d91964bb999226ec8379
Benoit Blanchon <github@benoitblanchon.fr>
2017-01-03 22:19:31 +0100

    Extracted architectures.js and calculator.js

---
+++ architectures.js
@@ -0,0 +1,46 @@
+var architectures = [
+  {
+    "name": "AVR 8-bit",
+    "array": {
+      "base": 4,
+      "item": 8
+    },
+    "object": {
+      "base": 4,
+      "item": 10
+    }
+  },
+  {
+    "name": "ESP8266",
+    "array": {
+      "base": 8,
+      "item": 12
+    },
+    "object": {
+      "base": 8,
+      "item": 16
+    }
+  },
+  {
+    "name": "Visual Studio x86",
+    "array": {
+      "base": 12,
+      "item": 24
+    },
+    "object": {
+      "base": 12,
+      "item": 32
+    }
+  },
+  {
+    "name": "Visual Studio x64",
+    "array": {
+      "base": 24,
+      "item": 24
+    },
+    "object": {
+      "base": 24,
+      "item": 32
+    }
+  }
+];
---
+++ calculator.js
@@ -0,0 +1,95 @@
+(function() {
+  function Recipe() {
+    var arrays = [];
+    var objects = [];
+
+    this.addJsonArray = function(size) {
+      if (arrays[size])
+        arrays[size]++;
+      else
+        arrays[size] = 1;
+    }
+
+    this.addJsonObject = function(size) {
+      if (objects[size])
+        objects[size]++;
+      else
+        objects[size] = 1;
+    }
+
+    this.getExpression = function() {
+      var elements = [];
+      for (var size in arrays) {
+        var count = arrays[size];
+        if (count > 1)
+          elements.push(count + "*JSON_ARRAY_SIZE("+size+")");
+        else
+          elements.push("JSON_ARRAY_SIZE("+size+")");
+      }
+      for (var size in objects) {
+        var count = objects[size];
+        if (count > 1)
+          elements.push(count + "*JSON_OBJECT_SIZE("+size+")");
+        else
+          elements.push("JSON_OBJECT_SIZE("+size+")");
+      }
+      return elements.join(" + ");
+    }
+
+    this.computeSize = function(arch) {
+      var totalBytes = 0;
+      for (var size in arrays) {
+        var count = arrays[size];
+        totalBytes += count*(arch.array.base + size*arch.array.item);
+      }
+      for (var size in objects) {
+        var count = objects[size];
+        totalBytes += count*(arch.object.base + size*arch.object.item);
+      }
+      return totalBytes;
+    }
+  }
+
+  function scanJson(recipe, obj) {
+    if (obj instanceof Array) {
+      recipe.addJsonArray(obj.length);
+      for (var i = 0; i<obj.length; i++)
+        scanJson(recipe, obj[i]);
+    }
+    else if (obj instanceof Object) {
+      recipe.addJsonObject(Object.keys(obj).length);
+      for (var key in obj)
+        scanJson(recipe, obj[key]);
+    }
+  }
+
+  input.oninput = function(e) {
+    results.style.display = 'none';
+    error.style.visibility = 'hidden';
+
+    try {
+      var recipe = new Recipe();
+      scanJson(recipe, JSON.parse(input.value));
+      var expression = recipe.getExpression();
+
+      $result_expr.innerText = expression;
+
+      for (var i=0; i<architectures.length; i++) {
+        var arch = architectures[i];
+        var row = $result_table.rows[i] || $result_table.insertRow(i);
+        var name_cell = row.cells[0] || row.insertCell(0);
+        var size_cell = row.cells[1] || row.insertCell(1);
+        name_cell.innerText = arch.name;
+        size_cell.innerText = recipe.computeSize(arch);
+      }
+      results.style.display = 'block';
+    }
+    catch (ex) {
+      error.innerText = "ERROR: " + ex.message;
+      error.style.visibility = 'visible';
+    }
+  }
+
+  input.value = "{\"sensor\":\"gps\",\"time\":1351824120,\"data\":[48.756080,2.302038]}";
+  input.oninput();
+})();
