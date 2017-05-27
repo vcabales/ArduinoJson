9f5a5a93fc8e4244072d1d257a37bbd9a2ac946c
Benoit Blanchon <github@benoitblanchon.fr>
2017-05-27 15:34:53 +0200

    Updated examples

--- _examples/http-server.ino
+++ _examples/http-server.ino
@@ -71,6 +71,8 @@ void loop() {
   if (client) {
     bool success = readRequest(client);
     if (success) {
+      // Use https://bblanchon.github.io/ArduinoJson/assistant/ to
+      // compute the right size for the buffer
       StaticJsonBuffer<500> jsonBuffer;
       JsonObject& json = prepareResponse(jsonBuffer);
       writeResponse(client, json);
